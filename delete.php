b0507b0171741cbdeb4ab3815c1b3560ab6b401a
Ahmad Faisal <81705914+ahmadfaissalll@users.noreply.github.com>
2022-01-24 01:55:37 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,18 +1,43 @@
 <?php
 
-require_once "conn.php";
+// CHECK SESSION
+require_once "checkSession.php";
 
-$id = $_POST['id'] ?? null;
+require_once "connProducts.php";
+
+$id = $_POST['id'];
 $imagePath = $_POST['image_path'];
+$requestUrl = $_POST["request_url"];
 
 if (!$id) {
     header("Location: " . "index.php");
     exit;
 }
 
-$sql = "delete from products where id = $id;";
+// var_dump($conn -> query($sql));
+
+$sql = "SELECT title FROM products WHERE id = $id";
+
+$result = $conn->query($sql);
+
+$products = $result->fetch_assoc();
+
+// JIKA PRODUK TIDAK ADA MAKA KELUAR DARI PROGRAM
+if ($products === null) {
+    exit;
+}
+
+
+$sql = "DELETE FROM products WHERE id = $id;";
 
 $conn -> query($sql);
+
+if ($conn -> affected_rows === 1) {
+    $_SESSION['deleteSuccess'] = "{$products['title']} Berhasil Dihapus";
+} else {
+    $_SESSION['deleteFail'] = "{$products['title']} Gagal Dihapus";
+}
+
 
 if ($imagePath !== "") {
 
@@ -24,4 +49,6 @@ if ($imagePath !== "") {
 
 $conn -> close();
 
-header("Location: " . "index.php");
+header("Location: " . $requestUrl);
+
+?>
